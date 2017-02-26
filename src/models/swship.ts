69ca6501b87ec105061d43671982433bdc20fd81
Roland Hof <roland.hof@gmail.com>
2017-02-26 02:33:13 +0100

    switch to angular 4

--- src/models/swship.ts
+++ src/models/swship.ts
@@ -6,18 +6,4 @@ export class SWShip
     cost: string;
     crew: string;
     url: string;
-
-    // get id():number {
-    //     let parts:string[] = this.url.split('/');
-    //     console.log('hello log');
-    //     parts.pop();
-    //     return +parts.pop();
-    // }
-
-    getId() : number
-    {
-        console.log('hello console');
-        let urlPart:string = this.url.split('/').pop();
-        return +urlPart;
-    }
 }
