a6e49a41208aaa696b3cfe5e20ef23fbbaae3b02
Roland Hof <roland.hof@gmail.com>
2017-03-01 00:19:37 +0100

    messing around with the navbar

--- src/components/navigation/navigation.ts
+++ src/components/navigation/navigation.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navigation',
   templateUrl: 'navigation.html',
-
+  styleUrls: ['navigation.css']
 })
 export class NavigationComponent 
 {
